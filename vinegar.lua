73b517c99fa355aefc6e2b6e8c6fb81194084c6b
Jonathan Porta <jonathan@jonathanporta.com>
2014-01-27 21:00:39 -0700

    Refactored the take damage function. Also, expanded prod to work on sleepers and other objects.

--- vinegar.lua
+++ vinegar.lua
@@ -11,6 +11,18 @@ PLUGIN.Version = "0.3"
 
 function PLUGIN:Init()
 
+	-- Read saved config
+	self.configFile = util.GetDatafile("vinegar")
+	local txt = self.configFile:GetText()
+	if (txt ~= "") then
+		self.config = json.decode(txt)
+	else
+		print("Vinegar config file missing. Falling back to default settings.")
+		self.config = {}
+		self.config.damage = 1000
+		self.config.vinegarForAll = true
+	end
+
 	-- List of users with Vinegar/prod enabled.
 	self.vinegarUsers = {}
 	self.prodUsers = {}
@@ -27,9 +39,15 @@ function PLUGIN:Init()
 	local FLAG_PROD = oxmin.AddFlag("canprod")
  
 	-- Register main chat command
-	oxminPlugin:AddExternalOxminChatCommand(self, "vinegar", {}, self.ToggleVinegar)
-	-- Uncomment this line if you want to require the canvinegar flag.
-	--oxminPlugin:AddExternalOxminChatCommand(self, "vinegar", {FLAG_VINEGAR}, self.ToggleVinegar)
+	if(self.config.vinegarForAll) then
+		-- If we are restricting to admins and flag holders
+		print("Vinegar enabled for all users.")
+		oxminPlugin:AddExternalOxminChatCommand(self, "vinegar", {}, self.ToggleVinegar)
+	else
+		-- If we want everyone to be able to use vinegar
+		print("Vinegar enabled for all flagged users only.")
+		oxminPlugin:AddExternalOxminChatCommand(self, "vinegar", {FLAG_VINEGAR}, self.ToggleVinegar)
+	end
 	oxminPlugin:AddExternalOxminChatCommand(self, "prod", {FLAG_PROD}, self.ToggleProd)
 
 	-- Read in Oxmin's stash of user infos.
@@ -41,17 +59,6 @@ function PLUGIN:Init()
 	else
 		self.data = {}
 		self.data.Users = {}
-	end
-
-	-- Read saved config
-	self.configFile = util.GetDatafile("vinegar")
-	local txt = self.configFile:GetText()
-	if (txt ~= "") then
-		self.config = json.decode(txt)
-	else
-		print("Vinegar config file missing. Falling back to default settings.")
-		self.config = {}
-		self.config.damage = 1000
 	end
 	
 	print("Vinegar plugin loaded - default damage set to: "..self.config.damage)
@@ -97,80 +104,128 @@ function PLUGIN:ToggleVinegar(netuser, args)
 	end
 end
 
--- *******************************************
--- PLUGIN:OnTakeDamage()
--- Called when an entity take damage
--- *******************************************
-local allStructures = util.GetStaticPropertyGetter(Rust.StructureMaster, 'AllStructures')
-local getStructureMasterOwnerId = util.GetFieldGetter(Rust.StructureMaster, "ownerID", true)
-
 function PLUGIN:ModifyDamage(takedamage, damage)
-	--TODO: This function is getting too long...
 	--print("vinegar.lua - PLUGIN:ModifyDamage(takedamage, damage)")
+	
+	-- lua rust bind lookups
+	local getStructureMasterOwnerId = util.GetFieldGetter(Rust.StructureMaster, "ownerID", true)
+	local getDeployableOwnerId = util.GetFieldGetter(Rust.DeployableObject, "ownerID", true)
 
-
-	--local char = takedamage:GetComponent("Character")
-	--local deployable = takedamage:GetComponent("DeployableObject")
+	local deployable = takedamage:GetComponent("DeployableObject")
 	local structureComponent = takedamage:GetComponent("StructureComponent")
 
-	--if (deployable) then
-		--print("trying to print deployable next")
-		--print(deployable)
-	--end
-	--return nil
+	if (deployable) then
+		-- A Deployable has been attacked!
+		-- Find the culprit
+		local attackerNetuser = self:GetDamageEventAttackerNetuser(damage)
+
+		if(attackerNetuser) then
+			if(self:CanProd(attackerNetuser)) then
+				-- find the deployable owner
+				local deployableOwnerId = getDeployableOwnerId(deployable)
+				-- do some prodding!
+				self:DoProd(attackerNetuser, deployableOwnerId)
+				-- Going to return here so we can prod without killing sleepers.
+				damage.amount = 0
+				return damage
+			end
+		end
+	end
+
 	if(structureComponent) then
 		-- A structure has been attacked!
+		-- Find the structureMaster obj
 		local structureMaster = structureComponent._master
-		local attacker = damage.attacker
-		local damageToTake = 0
+		-- Find the culprit
+		local attackerNetuser = self:GetDamageEventAttackerNetuser(damage)
 
-		-- TODO: This user to player to net to steam crap is messy.
-		if(attacker) then
-			local attackerClient = damage.attacker.client
-			if attackerClient then
-				local attackerUser = attackerClient.netUser
-				if(attackerUser) then
-					-- Attacker is another player!
-					-- Find the structure owner.
-					structureOwnerId = getStructureMasterOwnerId(structureMaster)
-					structureOwnerSteamId = rust.CommunityIDToSteamID(structureOwnerId)
+		if(attackerNetuser) then
+			-- Find the structure owner.
+			structureOwnerId = getStructureMasterOwnerId(structureMaster)
+			structureOwnerSteamId = rust.CommunityIDToSteamID(structureOwnerId)
 
-					-- Figure out if the attacker is allowed to cause damage.
-					attackerSteamId = self:NetuserToSteamID(attackerUser)
-					if(self.vinegarUsers[attackerSteamId]) then
-						-- vinegar is on, but who's stuff are we messing with?
-						if(structureOwnerSteamId == attackerSteamId) then
-							--destroying your own stuff? Ok.
-							damage.amount = self.config.damage
-							return damage
-						else
-							-- Only admins can destroy other's things for now!
-							oxminPluginInstance = cs.findplugin("oxmin")
-							if(oxminPluginInstance.HasFlag(oxminPluginInstance, attackerUser, oxmin.AddFlag("godmode"), true)) then
-								damage.amount = self.config.damage
-								return damage
-							else
-								rust.Notice(attackerUser, "This is not yours!")
-							end
-						end
-					end
-					-- Prod Implementation
-					if(self.prodUsers[attackerSteamId]) then
-						if(self.data.Users[""..structureOwnerId]) then
-							local details = self.data.Users[""..structureOwnerId]
-							rust.Notice(attackerUser, "This is owned by "..details.Name.."!")
-						else
-							rust.Notice(attackerUser, "Sorry, don't know who owns this...")
-						end
-					end
-				end
+			-- Figure out if the attacker is allowed to cause damage.
+			if(self:CanDamage(attackerNetuser, structureOwnerSteamId)) then
+				damage.amount = self.config.damage
+				return damage
 			end
+
+			-- do some prodding!
+			self:DoProd(attackerNetuser, structureOwnerId)
 		end
 	end	
 end
+
+function PLUGIN:DoProd(attackerNetuser, ownerId)
+	-- Prod Implementation
+	local ownerDetails = self:GetUserDetailsByCommunityId(ownerId)
+	if(ownerDetails) then
+		rust.Notice(attackerNetuser, "This is owned by "..ownerDetails.Name.."!")
+	else
+		rust.Notice(attackerNetuser, "Sorry, don't know who owns this...")
+	end
+end
+
+function PLUGIN:CanProd(attackerNetuser)
+	-- Is this person allowed to prod?
+	local attackerSteamId = self:NetuserToSteamID(attackerNetuser)
+	if(self.prodUsers[attackerSteamId]) then
+		return true
+	end
+	return false
+end
+
+function PLUGIN:CanDamage(attackerNetuser, ownerSteamId)
+	-- return true if attacker is allowed to damage this owner's things
+	local attackerSteamId = self:NetuserToSteamID(attackerNetuser)
+	if(self.vinegarUsers[attackerSteamId]) then
+		-- vinegar is on, but who's stuff are we messing with?
+		if(structureOwnerSteamId == attackerSteamId) then
+			-- destroying your own stuff? Proceed.
+			return true
+		else
+			-- Only admins can destroy other's things for now!
+			local oxminPluginInstance = cs.findplugin("oxmin")
+			if(oxminPluginInstance.HasFlag(oxminPluginInstance, attackerNetuser, oxmin.AddFlag("godmode"), true)) then
+				return true
+			end
+		end
+	end
+	-- we didn't meet any of the conditions, no destruction for you.
+	return false
+end
+
+----
+-- User lookup utility functions
+----
 
 function PLUGIN:NetuserToSteamID(netuser)
 	userID = rust.GetUserID(netuser)
 	steamID = rust.CommunityIDToSteamID(tonumber(userID))
 	return steamID
+end
+
+function PLUGIN:GetUserDetailsByCommunityId(communityId)
+	--return user details, or nil if not found.
+	local details = self.data.Users[""..communityId] --Is there a better way to change to string?
+	if(details) then
+		return details
+	end
+	return nil
+end
+
+function PLUGIN:GetDamageEventAttackerNetuser(damage)
+	-- Get netuser of an attacking user. If attacker is not a user, return nil
+	local attacker = damage.attacker
+
+	if(attacker) then
+		local attackerClient = damage.attacker.client
+		if attackerClient then
+			local attackerNetuser = attackerClient.netUser
+			if(attackerNetuser) then
+				return attackerNetuser
+			end
+		end
+	end
+	return nil
 end
